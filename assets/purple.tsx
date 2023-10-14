55ddbbfba233005247b62cf290aeffa98692aafc
Bryan Klimt <bklimt@gmail.com>
2023-10-14 13:20:42 -0700

    add green purple terrain connectors

--- assets/purple.tsx
+++ assets/purple.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="purple" tilewidth="8" tileheight="8" tilecount="256" columns="16" backgroundcolor="#3d3846">
+<tileset version="1.10" tiledversion="1.10.2" name="purple" tilewidth="8" tileheight="8" tilecount="256" columns="16" backgroundcolor="#3d3846">
  <image source="purple.png" width="128" height="128"/>
  <wangsets>
   <wangset name="Purple Edges" type="edge" tile="-1">
@@ -11,19 +11,33 @@
    <wangtile tileid="3" wangid="0,0,0,0,1,0,0,0"/>
    <wangtile tileid="4" wangid="1,0,1,0,1,0,1,0"/>
    <wangtile tileid="5" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="8" wangid="2,0,1,0,1,0,2,0"/>
+   <wangtile tileid="9" wangid="2,0,1,0,1,0,1,0"/>
+   <wangtile tileid="10" wangid="2,0,2,0,1,0,1,0"/>
+   <wangtile tileid="11" wangid="2,0,2,0,1,0,2,0"/>
    <wangtile tileid="16" wangid="1,0,1,0,1,0,0,0"/>
    <wangtile tileid="17" wangid="1,0,1,0,1,0,1,0"/>
    <wangtile tileid="18" wangid="1,0,0,0,1,0,1,0"/>
    <wangtile tileid="19" wangid="1,0,0,0,1,0,0,0"/>
    <wangtile tileid="20" wangid="1,0,1,0,1,0,1,0"/>
    <wangtile tileid="21" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="24" wangid="1,0,1,0,1,0,2,0"/>
+   <wangtile tileid="26" wangid="1,0,2,0,1,0,1,0"/>
+   <wangtile tileid="27" wangid="1,0,2,0,1,0,2,0"/>
    <wangtile tileid="32" wangid="1,0,1,0,0,0,0,0"/>
    <wangtile tileid="33" wangid="1,0,1,0,0,0,1,0"/>
    <wangtile tileid="34" wangid="1,0,0,0,0,0,1,0"/>
    <wangtile tileid="35" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="40" wangid="1,0,1,0,2,0,2,0"/>
+   <wangtile tileid="41" wangid="1,0,1,0,2,0,1,0"/>
+   <wangtile tileid="42" wangid="1,0,2,0,2,0,1,0"/>
+   <wangtile tileid="43" wangid="1,0,2,0,2,0,2,0"/>
    <wangtile tileid="48" wangid="0,0,1,0,0,0,0,0"/>
    <wangtile tileid="49" wangid="0,0,1,0,0,0,1,0"/>
    <wangtile tileid="50" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="56" wangid="2,0,1,0,2,0,2,0"/>
+   <wangtile tileid="57" wangid="2,0,1,0,2,0,1,0"/>
+   <wangtile tileid="58" wangid="2,0,2,0,2,0,1,0"/>
    <wangtile tileid="64" wangid="0,0,2,0,2,0,0,0"/>
    <wangtile tileid="65" wangid="0,0,2,0,2,0,2,0"/>
    <wangtile tileid="66" wangid="0,0,0,0,2,0,2,0"/>
@@ -52,20 +66,34 @@
    <wangtile tileid="2" wangid="0,0,0,0,0,1,0,0"/>
    <wangtile tileid="4" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="5" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="8" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="9" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="10" wangid="0,2,0,2,0,1,0,2"/>
    <wangtile tileid="16" wangid="0,1,0,1,0,0,0,0"/>
    <wangtile tileid="17" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="18" wangid="0,0,0,0,0,1,0,1"/>
    <wangtile tileid="20" wangid="0,1,0,1,0,1,0,1"/>
    <wangtile tileid="21" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="24" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="26" wangid="0,2,0,2,0,1,0,1"/>
    <wangtile tileid="32" wangid="0,1,0,0,0,0,0,0"/>
    <wangtile tileid="33" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="34" wangid="0,0,0,0,0,0,0,1"/>
    <wangtile tileid="36" wangid="0,1,0,0,0,1,0,1"/>
    <wangtile tileid="37" wangid="0,1,0,1,0,0,0,1"/>
    <wangtile tileid="38" wangid="0,0,0,1,0,0,0,1"/>
+   <wangtile tileid="40" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="41" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="42" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="44" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="45" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="46" wangid="0,2,0,1,0,2,0,1"/>
    <wangtile tileid="52" wangid="0,0,0,1,0,1,0,1"/>
    <wangtile tileid="53" wangid="0,1,0,1,0,1,0,0"/>
    <wangtile tileid="54" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="60" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="61" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="62" wangid="0,1,0,2,0,1,0,2"/>
    <wangtile tileid="64" wangid="0,0,0,2,0,0,0,0"/>
    <wangtile tileid="65" wangid="0,0,0,2,0,2,0,0"/>
    <wangtile tileid="66" wangid="0,0,0,0,0,2,0,0"/>
