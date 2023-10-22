dc043af0f7cf282bd52c00854517bf3254e6c005
Bryan Klimt <bklimt@gmail.com>
2023-10-21 23:17:48 -0700

    fix platform tile bug

--- platforms.py
+++ platforms.py
@@ -38,7 +38,7 @@ class Platform:
 
         self.id = obj.id
         self.tileset = tileset
-        self.tile_id = obj.gid
+        self.tile_id = obj.gid - 1
         self.x = obj.x * 16
         self.y = obj.y * 16
         self.dx = 0
