f8aa79cb6ead2c8fd242b9e91acdddea8068d345
Bryan Klimt <bklimt@gmail.com>
2023-11-01 23:10:39 -0700

    fix KillScreen draw method

--- kill.py
+++ kill.py
@@ -4,11 +4,12 @@ import typing
 
 from imagemanager import ImageManager
 from inputmanager import InputManager
+from renderoptions import RenderOptions
 from scene import Scene
 from soundmanager import SoundManager
 
 
-class KillScreen(Scene):
+class KillScreen:
     previous: Scene
     next: typing.Callable[[], Scene]
 
@@ -16,16 +17,17 @@ class KillScreen(Scene):
         self.previous = previous
         self.next = next
 
-    def update(self, input: InputManager, sounds: SoundManager) -> Scene:
-        if input.is_ok_triggered():
+    def update(self, inputs: InputManager, sounds: SoundManager) -> Scene:
+        if inputs.is_ok_triggered():
             return self.next()
         return self
 
-    def draw(self, surface: pygame.Surface, dest: pygame.Rect, images: ImageManager):
+    def draw(self, surface: pygame.Surface, dest: pygame.Rect, images: ImageManager, options: RenderOptions):
         red_color = pygame.Color(255, 0, 0, 127)
         red_surface = pygame.Surface(dest.size, pygame.SRCALPHA)
         red_surface.fill(red_color, dest)
-        self.previous.draw(surface, dest, images)
+        self.previous.draw(surface, dest, images, options)
+        options.spotlight_enabled = False
         surface.blit(red_surface, dest)
 
         text = "DEAD"
