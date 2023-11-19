f23e15267b1292ac3b3a4d8dc7519abaf36eb3ff
Bryan Klimt <bklimt@gmail.com>
2023-11-18 16:09:47 -0800

    work on pastel tileset

--- assets/pastel.tsx
+++ assets/pastel.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.0" name="pastel" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+<tileset version="1.10" tiledversion="1.10.2" name="pastel" tilewidth="8" tileheight="8" tilecount="256" columns="16">
  <image source="pastel.png" width="128" height="128"/>
  <tile id="4">
   <properties>
@@ -30,9 +30,77 @@
    <property name="deadly" type="bool" value="true"/>
   </properties>
  </tile>
+ <tile id="168">
+  <properties>
+   <property name="oneway" value="E"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="oneway" value="W"/>
+  </properties>
+ </tile>
+ <tile id="170">
+  <properties>
+   <property name="convey" value="E"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="171">
+  <properties>
+   <property name="convey" value="E"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="172">
+  <properties>
+   <property name="convey" value="W"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="173">
+  <properties>
+   <property name="convey" value="W"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
  <tile id="176">
   <properties>
    <property name="star" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="oneway" value="S"/>
+  </properties>
+ </tile>
+ <tile id="185">
+  <properties>
+   <property name="oneway" value="N"/>
+  </properties>
+ </tile>
+ <tile id="186">
+  <properties>
+   <property name="convey" value="E"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="187">
+  <properties>
+   <property name="convey" value="E"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="convey" value="W"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="189">
+  <properties>
+   <property name="convey" value="W"/>
+   <property name="solid" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="192">
@@ -42,13 +110,101 @@
  </tile>
  <tile id="193">
   <properties>
+   <property name="direction" value="N"/>
+   <property name="distance" type="int" value="0"/>
+   <property name="overflow" value="oscillate"/>
    <property name="platform" type="bool" value="true"/>
+   <property name="speed" type="int" value="4"/>
+  </properties>
+ </tile>
+ <tile id="202">
+  <properties>
+   <property name="button" type="bool" value="true"/>
+   <property name="button_type" value="toggle"/>
+   <property name="color" value="pastel_orange"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="203">
+  <properties>
+   <property name="button" type="bool" value="true"/>
+   <property name="button_type" value="toggle"/>
+   <property name="color" value="pastel_blue"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="204">
+  <properties>
+   <property name="alternate" type="int" value="206"/>
+   <property name="condition" value="pastel_orange"/>
+  </properties>
+ </tile>
+ <tile id="205">
+  <properties>
+   <property name="alternate" type="int" value="207"/>
+   <property name="condition" value="pastel_blue"/>
+  </properties>
+ </tile>
+ <tile id="206">
+  <properties>
+   <property name="alternate" type="int" value="204"/>
+   <property name="condition" value="!pastel_orange"/>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="207">
+  <properties>
+   <property name="alternate" type="int" value="205"/>
+   <property name="condition" value="!pastel_blue"/>
+   <property name="solid" type="bool" value="false"/>
   </properties>
  </tile>
  <tile id="208">
   <properties>
    <property name="solid" type="bool" value="true"/>
    <property name="spring" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="218">
+  <properties>
+   <property name="button" type="bool" value="true"/>
+   <property name="button_type" value="toggle"/>
+   <property name="color" value="pastel_pink"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="219">
+  <properties>
+   <property name="button" type="bool" value="true"/>
+   <property name="button_type" value="toggle"/>
+   <property name="color" value="pastel_green"/>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="220">
+  <properties>
+   <property name="alternate" type="int" value="222"/>
+   <property name="condition" value="pastel_pink"/>
+  </properties>
+ </tile>
+ <tile id="221">
+  <properties>
+   <property name="alternate" type="int" value="223"/>
+   <property name="condition" value="pastel_green"/>
+  </properties>
+ </tile>
+ <tile id="222">
+  <properties>
+   <property name="alternate" type="int" value="220"/>
+   <property name="condition" value="!pastel_pink"/>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="223">
+  <properties>
+   <property name="alternate" type="int" value="221"/>
+   <property name="condition" value="!pastel_green"/>
+   <property name="solid" type="bool" value="false"/>
   </properties>
  </tile>
  <tile id="240">
